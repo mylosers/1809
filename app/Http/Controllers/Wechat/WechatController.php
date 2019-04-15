0095205af2836a51362e4818b805c124b5f39744
losers <losers@localhost.localdomain>
2019-04-15 09:07:18 +0800

    access_token

---
+++ app/Http/Controllers/Wechat/WechatController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Http\Controllers\Wechat;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use GuzzleHttp;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\Redis;
+use App\Model\WeixinUser;
+use App\Model\WeixinChatModel;
+use App\Model\WeixinMedia;
+
+
+class WechatController extends Controller
+{
+    protected $redis_weixin_access_token = 'str:weixin_access_token';     //微信 access_token
+
+    /**
+     * 首次接入
+     */
+    public function getEvent()
+    {
+        //$get = json_encode($_GET);
+        //$str = '>>>>>' . date('Y-m-d H:i:s') .' '. $get . "<<<<<\n";
+        //file_put_contents('logs/weixin.log',$str,FILE_APPEND);
+        echo $_GET['echostr'];
+    }
+
+    /**
+     * 接收微信服务器事件推送
+     */
+    public function wxEvent()
+    {
+        $data = file_get_contents("php://input");
+        //解析XML
+        $xml = simplexml_load_string($data);        //将 xml字符串 转换成对象
+        //记录日志
+        $log_str = date('Y-m-d H:i:s') . "\n" . $data . "\n<<<<<<<";
+        file_put_contents('logs/wx_event.log', $log_str, FILE_APPEND);
+    }
+
+
+    /**
+     * 缓存access_token
+     */
+    public function access_token()
+    {
+        //获取缓存
+        $token = Redis::get($this->redis_weixin_access_token);
+        if (!$token) {        // 无缓存 请求微信接口
+            $url = 'https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=' . env('WX_APPID') . '&secret=' . env('WX_APPSECRET');
+            $data = json_decode(file_get_contents($url), true);
+            //记录缓存
+            $token = $data['access_token'];
+            Redis::set($this->redis_weixin_access_token, $token);
+            Redis::setTimeout($this->redis_weixin_access_token, 3600);
+        }
+        return $token;
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/wechat/url','Wechat\WechatController@getEvent');   //接入
+Route::post('/wechat/url','Wechat\WechatController@wxEvent');   //接入
+Route::get('/wechat/access_token','Wechat\WechatController@access_token');   //获取access_token
+
